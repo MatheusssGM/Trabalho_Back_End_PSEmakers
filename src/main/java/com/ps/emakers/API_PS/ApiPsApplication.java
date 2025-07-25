1c45699890341c20b5ee576a467de732b3e3f007
Matheus G <mat.gm2502@gmail.com>
2025-07-25 16:25:22 -0300

    add : estrutura inicial utilizando o Spring initializr

---
+++ src/main/java/com/ps/emakers/API_PS/ApiPsApplication.java
@@ -0,0 +1,13 @@
+package com.ps.emakers.API_PS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/emakers/API_PS/ApiPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.emakers.API_PS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
